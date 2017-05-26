0ecab9b65d01b4f19b5a4bca037ca108d560ca86
HT413 <htran413@gmail.com>
2017-05-25 18:29:20 -0700

    Initial commit

---
+++ Scene/Window.cpp
@@ -0,0 +1,127 @@
+#include "Window.h"
+#include "Shader.hpp"
+
+// Window width and height
+int width, height;
+
+// For the trackball
+double lastX, lastY;
+enum trackballAction { NO_ACTION, C_ROTATE };
+trackballAction mouseAction;
+
+// Other variables
+vec3 eye(0, 0, 0), cam_pos(0, 0, -1), cam_up(0, 1, 0);
+mat4 projection, view;
+
+GLFWwindow* createWindow(int w, int h){
+	// Initialize GLFW
+	if(!glfwInit())
+	{
+		cerr << "Failed to initialize GLFW!" << endl;
+		PROGERR(30);
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+	// Create the GLFW window
+	GLFWwindow* window = glfwCreateWindow(w, h, WINDOW_TITLE, 0, 0);
+
+	// Check if the window could not be created
+	if(!window)
+	{
+		cerr << "Failed to open GLFW window!" << endl;
+		glfwTerminate();
+		PROGERR(31);
+	}
+
+	// Make the context of the window
+	glfwMakeContextCurrent(window);
+
+	// Set swap interval to 1
+	glfwSwapInterval(1);
+
+	// Get the width and height of the framebuffer to properly resize the window
+	glfwGetFramebufferSize(window, &width, &height);
+	// Call the resize callback to make sure things get drawn immediately
+	resizeCallback(window, width, height);
+
+	return window;
+}
+
+void initObjects(){
+
+}
+
+void destroyObjects(){
+
+}
+
+void resizeCallback(GLFWwindow* window, int w, int h){
+	width = w;
+	height = h;
+	// Set the viewport size
+	glViewport(0, 0, width, height);
+
+	if(height > 0)
+	{
+		projection = perspective(PI / 2.f, (float)width / (float)height, 0.1f, 1000.0f);
+		view = lookAt(eye, cam_pos, cam_up);
+	}
+}
+
+void update(){
+
+}
+
+void displayCallback(GLFWwindow* window){
+	// Draw
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glfwSwapBuffers(window);
+
+	glfwPollEvents();
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if(action == GLFW_PRESS) {
+		switch(key) {
+		case GLFW_KEY_ESCAPE:
+			glfwSetWindowShouldClose(window, GL_TRUE);
+			break;
+		}
+	}
+}
+
+void cursorCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	// Camera rotation
+	if(mouseAction == C_ROTATE) {
+		float angle;
+		// Perform horizontal (y-axis) rotation
+		angle = (float)(lastX - xpos) / 100.0f;
+		cam_pos = vec3(rotate(mat4(1.0f), angle, vec3(0.0f, 1.0f, 0.0f)) * vec4(cam_pos, 1.0f));
+		cam_up = vec3(rotate(mat4(1.0f), angle, vec3(0.0f, 1.0f, 0.0f)) * vec4(cam_up, 1.0f));
+		//Now rotate vertically based on current orientation
+		angle = (float)(ypos - lastY) / 100.0f;
+		vec3 axis = cross(eye - cam_pos, cam_up);
+		cam_pos = vec3(rotate(mat4(1.0f), angle, axis) * vec4(cam_pos, 1.0f));
+		cam_up = vec3(rotate(mat4(1.0f), angle, axis) * vec4(cam_up, 1.0f));
+		// Now update the camera
+		view = lookAt(eye, cam_pos, cam_up);
+		lastX = xpos;
+		lastY = ypos;
+	}
+}
+
+void mouseCallback(GLFWwindow* window, int button, int action, int mods)
+{
+	if(action == GLFW_RELEASE) {
+		mouseAction = NO_ACTION;
+	}
+	if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		glfwGetCursorPos(window, &lastX, &lastY);
+		mouseAction = C_ROTATE;
+	}
+}
---
+++ Scene/Window.h
@@ -0,0 +1,16 @@
+#ifndef _GRAPHICS_WINDOW_H
+#define _GRAPHICS_WINDOW_H
+
+#include "main.h"
+
+void initObjects();
+void destroyObjects();
+GLFWwindow* createWindow(int, int);
+void resizeCallback(GLFWwindow*, int, int);
+void update();
+void displayCallback(GLFWwindow*);
+void keyCallback(GLFWwindow*, int, int, int, int);
+void cursorCallback(GLFWwindow*, double, double);
+void mouseCallback(GLFWwindow*, int, int, int);
+
+#endif
